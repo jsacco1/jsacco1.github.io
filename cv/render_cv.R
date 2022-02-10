f712fd43568042940e98308947093eb3c7578ebe
jsacco1 <jsacco001@gmail.com>
2022-02-10 12:13:42 -0800

    render_cv.R creates both pdf and html

---
+++ cv/render_cv.R
@@ -0,0 +1,14 @@
+# Knit the HTML version
+rmarkdown::render("index.Rmd",
+                  params = list(pdf_mode = FALSE),
+                  output_file = "James_Sacco_CV.html")
+
+# Knit the PDF version to temporary html location
+tmp_html_cv_loc <- fs::file_temp(ext = ".html")
+rmarkdown::render("index.Rmd",
+                  params = list(pdf_mode = TRUE),
+                  output_file = tmp_html_cv_loc)
+
+# Convert to PDF using Pagedown
+pagedown::chrome_print(input = tmp_html_cv_loc,
+                       output = "James_Sacco_CV.pdf")
